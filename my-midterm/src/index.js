7bddd50326bb085a6703860d18bbc11589602b82
ameeks96 <144856965+Ameeksin@users.noreply.github.com>
2023-10-17 21:31:19 -0400

    v1.3

--- my-midterm/src/index.js
+++ my-midterm/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddProduct from './components/AddProduct/AddProduct';
+import SignUpUser from './components/SignupUser/SignUpUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     {<AddProduct/>}
+      {<SignUpUser/>}
+
   </React.StrictMode>
 );
 
